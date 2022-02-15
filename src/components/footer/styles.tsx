b01c01fdffd87c69b4659222219c9b8ec896453a
Matheus Sodre <matheusssodre98@gmail.com>
2022-02-15 17:58:33 -0300

    Add image

--- src/components/footer/styles.tsx
+++ src/components/footer/styles.tsx
@@ -2,8 +2,11 @@ import styled from 'styled-components'
 import * as T from './types'
 
 export const Footer = styled.div<T.Footer>`
-  background: black;
+  background: rgb(192, 45, 26);
   height: ${({ height }) => height};
   width: ${({ width }) => width};
   color: white;
+  box-sizing: border-box;
+  display: flex;
+  margin-bottom: 2em;
 `
